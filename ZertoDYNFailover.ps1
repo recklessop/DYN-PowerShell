4bd6714fb9e0e5fd41168280e663151e24cdb075
Justin Paul <justin@jpaul.me>
2017-07-26 16:10:04 -0400

    Changed Script Name

--- ZertoDYNFailover.ps1
+++ ZertoDYNFailover.ps1
@@ -36,18 +36,27 @@ $token = $result.data.token
 #Create the header with the session token data for subsequent Dynect calls
 $headers = @{"Auth-Token" = "$token"}
 
+$records = (Invoke-RestMethod -Uri "https://api.dynect.net/REST/AllRecord/$zone/" -Method GET -ContentType "application/json" -Headers $headers).data
 
+#Split Results and list just the ARecords
+     $records = $records.split(" ")
+     $records | foreach {
+        $items = $_.split("/")
+        if ($items[2] -eq "ARecord"){Echo $items[4]}
+     }
 
 
 Write-Host "FQDNs will be updated as follows:"
 foreach ($node in $DynNodes)
 {
     $node | Format-Table -Auto
+
+
+
+
 }
-
-
 
 
 write-host "Logout"
 #Remove the session token/logout and suppress any output
-Invoke-RestMethod -Uri "https://api.dynect.net/REST/Session" -Method Delete -ContentType "application/json" -Headers $headers
+Invoke-RestMethod -Uri "https://api.dynect.net/REST/Session" -Method Delete -ContentType "application/json" -Headers $headers | out-null
