3b54f1e1cda9ad3ee7126157211802c3dcff599e
Justin Paul <justin@jpaul.me>
2017-07-25 22:24:16 -0400

    Created List Nodes PS Script

---
+++ dynListNodes.ps1
@@ -0,0 +1,56 @@
+#####################################################################################
+# Created by Justin Paul, Zerto Tech Alliances Architect, 2017                      #
+#                                                                                   #
+# Original script forked from:                                                      #
+# http://www.jjclements.co.uk/2015/12/04/powershell-script-for-dyn-dynect-rest-api/ #
+#                                                                                   #
+#This script/function updates a node's DNS A record in Dynect                       #
+#for the predefined zone using the Dynect REST API                                  #
+#usage: dynListNodes.ps1 <nodename>                                                 #
+#####################################################################################
+ 
+####################################
+#Start of user changeable variables#
+####################################
+$customername = "mycompanyname"
+$username = "james"
+$password = "guessme"
+$zone = "foo.bar"
+##################################
+#End of user changeable variables#
+##################################
+ 
+function dynListNodes
+{
+     param([string]$node)
+ 
+     try
+     {
+ 
+     
+#Format credentials accordingly for the body content of the initial request below
+     $credentials = '{"customer_name":"' + $customername + '","user_name":"' + $username + '","password":"' + $password + '"}'
+ 
+     
+#Use credentials to request and store a session token from Dynect for later use
+     $result = Invoke-RestMethod -Uri https://api2.dynect.net/REST/Session -Method Post -ContentType "application/json" -Body $credentials
+     $token = $result.data.token
+ 
+ 
+#Create the header with the session token data for subsequent Dynect calls
+     $headers = @{"Auth-Token" = "$token"}
+ 
+     
+#Get Existing Node Records for the Zone
+     Invoke-RestMethod -Uri "https://api2.dynect.net/REST/AllRecord/$zone/" -Method GET -ContentType "application/json" -Headers $headers
+ 
+     
+#Remove the session token/logout and suppress any output
+     Invoke-RestMethod -Uri "https://api2.dynect.net/REST/Session" -Method Delete -ContentType "application/json" -Headers $headers | out-null
+ 
+     }
+ 
+     catch
+     {
+     }
+}
