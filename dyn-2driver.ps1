93c561642bc2ca858f886e599122824061ac8fab
Justin Paul <justin@jpaul.me>
2017-07-26 14:27:04 -0400

    More Testing Edxamples

---
+++ dyn-2driver.ps1
@@ -0,0 +1,18 @@
+﻿
+Import-Module -Name .\dyn-functions.psm1
+
+
+$token = dynLogIn ".\config.txt"
+
+$zone = "labrack.xyz123"
+$type = "ARecord"
+
+$mynodes = dynGetNodes $type $zone $token
+
+
+foreach ($node in $mynodes) 
+{
+    Write-Host $node "`n"
+}
+
+dynLogOut $token
