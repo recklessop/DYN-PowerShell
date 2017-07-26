3c1547fa9a168073ec591b213da1af98ba83e000
Justin Paul <justin@jpaul.me>
2017-07-26 15:09:15 -0400

    added login script to customobject.ps1

--- customobject.ps1
+++ customobject.ps1
@@ -24,6 +24,21 @@ $DynNodes =
 ##################################
 #End of user changeable variables#
 ##################################
+
+# Login to DYN API and get Auth Token
+#Format credentials accordingly for the body content of the initial request below
+$credentials = '{"customer_name":"' + $customername + '","user_name":"' + $username + '","password":"' + $password + '"}'
+ 
+#Use credentials to request and store a session token from Dynect for later use
+$result = Invoke-RestMethod -Uri https://api.dynect.net/REST/Session -Method Post -ContentType "application/json" -Body $credentials
+$token = $result.data.token
+
+#Create the header with the session token data for subsequent Dynect calls
+$headers = @{"Auth-Token" = "$token"}
+
+
+
+
 Write-Host "FQDNs will be updated as follows:"
 foreach ($node in $DynNodes)
 {
