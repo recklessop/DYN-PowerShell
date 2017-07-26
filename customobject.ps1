6a80b707844b48e6a905a826ba6081342c3eda65
Justin Paul <justin@jpaul.me>
2017-07-26 15:05:19 -0400

    building customobject script for zerto failover script without functions.

--- customobject.ps1
+++ customobject.ps1
@@ -1,18 +1,38 @@
+
+
+
+####################################
+#Start of user changeable variables#
+####################################
+
+###########################
+# DYN API Credentials     #
+###########################
+$customername = "zerto"
+$username = "jpaul"
+$password = "z0@xzxCk58"
+
 ############################################
 # Start of Server ARecord List for changes #
 ############################################
 $DynNodes = 
-@([pscustomobject]@{fqdn="server1.labrack.xyz";ip="1.1.1.1"},
-[pscustomobject]@{fqdn="server2.labrack.xyz";ip="2.2.2.2"},
-[pscustomobject]@{fqdn="server3.labrack.xyz";ip="3.3.3.3"})
-############################################
-# Start of Server ARecord List for changes #
-############################################
+@([pscustomobject]@{fqdn="server1.labrack.xyz";ip="1.1.1.1";record_id=""},
+[pscustomobject]@{fqdn="server2.labrack.xyz";ip="2.2.2.2";record_id=""},
+[pscustomobject]@{fqdn="server3.labrack.xyz";ip="3.3.3.3";record_id=""})
 
 
+##################################
+#End of user changeable variables#
+##################################
+Write-Host "FQDNs will be updated as follows:"
 foreach ($node in $DynNodes)
 {
     $node | Format-Table -Auto
 }
 
-write-host "Finished"
+
+
+
+write-host "Logout"
+#Remove the session token/logout and suppress any output
+Invoke-RestMethod -Uri "https://api.dynect.net/REST/Session" -Method Delete -ContentType "application/json" -Headers $headers
--- dynListNodes.ps1
+++ dynListNodes.ps1
@@ -23,9 +23,9 @@ $zone = "labrack.xyz"
 # Start of Server ARecord List for changes #
 ############################################
 $DynNodes = 
-@([pscustomobject]@{fqdn="server1.labrack.xyz";ip="1.1.1.1"},
-[pscustomobject]@{fqdn="server2.labrack.xyz";ip="2.2.2.2"},
-[pscustomobject]@{fqdn="server3.labrack.xyz";ip="3.3.3.3"})
+@([pscustomobject]@{fqdn="server1.labrack.xyz";ip="1.1.1.1";record_id=""},
+[pscustomobject]@{fqdn="server2.labrack.xyz";ip="2.2.2.2";record_id=""},
+[pscustomobject]@{fqdn="server3.labrack.xyz";ip="3.3.3.3";record_id=""})
 ############################################
 # Start of Server ARecord List for changes #
 ############################################
